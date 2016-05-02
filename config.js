84ce8763897d9016ee7b8f7913383745794bb4e1
johnqiang <zqfly2013@gmail.com>
2016-05-02 00:45:51 -0400

    Added Twitter API, realized search query

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'yMJ4PyU6wzasCIa0wHKtrpVTn',
+	consumer_secret:      'ZjLihdaSxg4wxc5csn8MPrCJ3CE2FiBMcZw9NQQQO82hPdse5C',
+	access_token:         '3330431387-iFa2TNFhZVpbGy0DdftL4xulrYKbOMAlpkdAHLv',
+	access_token_secret:  'HjoNbWGaI41LjhjvAFtiYZfjkP8vE0R7h7iN9sAfNHyoU'
+};
